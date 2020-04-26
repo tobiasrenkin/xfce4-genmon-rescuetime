8cc7d645f626690835e64ce46962a4472237c022
Tobias Renkin <tobias.renkin@gmail.com>
2020-04-26 22:31:43 +0200

    updates

--- app.py
+++ app.py
@@ -1,16 +1,25 @@
 #!/usr/bin/env python3
 
 import requests
-import datetime
+import datetime, time
 import pprint
 import json
 import re
 import sys, os
 
+### basic definitions
 sdir = os.getenv("HOME")+"/.config/xfce4-genmon-rescuetime"
 adir = "/".join(sys.path[0].split("/")[:])
+now = datetime.datetime.now()
+today = datetime.datetime.today().strftime('%Y-%m-%d')
+tomorrow = (datetime.date.today() + datetime.timedelta(days=1)).strftime('%Y-%m-%d')
 
-# check if key is set up
+########################################################################
+
+def quitwithna(text="NA"):
+	print("<txtclick>python3 "+adir+"/chart.py</txtclick><txt><span weight='bold'><span fgcolor="+cfg["colors"]["text"]+">"+text+"</span></span></txt>")
+	sys.exit()
+
 def askkey():
 	import tkinter as tk
 	from tkinter import simpledialog as sd
@@ -20,89 +29,144 @@ def askkey():
 	tkinput.destroy()
 	return key
 
-if not(os.path.exists(sdir)):
-	os.mkdir(sdir)
+def api_request(key, interval, start, end):
+	# get basic json data
+	rspec = {"format": "json", "by": "interval", "rk": "productivity", "key": key,
+		"interval": interval,
+		"rb": start,
+		"re": end
+	}
+	rstr = "https://www.rescuetime.com/anapi/data" + "?" + "&".join([k+"="+rspec[k] for k in rspec.keys()])
+
+	try:
+		apianswer = requests.get(rstr).text
+	except:
+		raise URLError
+
+	rawdata = json.loads(apianswer)
+	
+	# reformat to [%Y-m-dT%H:%M:%S, productivity, minutes]; Add interval (hours, minutes, etc.) and time stamp
+	data = {}
+	data["interval"] = interval
+	data["timestamp"] = time.time()
+	data["rows"] = []
+	for r in rawdata["rows"]:
+		data["rows"].append([r[0], r[-1], r[1]/60])
+	return data
+
+
+########################################################################
+
+# load settings, else use default
+with open(adir+"/defaultcfg", "r") as f:
+		defaultcfg = json.loads(f.read())
+
+if os.path.exists(sdir+"/settings"):
+	with open(sdir+"/settings", "r") as f:
+		cfg = json.loads(f.read())
+else:
+	cfg = defaultcfg
+		
+if "key" not in cfg:
+	cfg["key"] = askkey()
+elif "key" == "":
+	cfg["key"] = askkey()
+
+cfg["app"].setdefault("start_hour", defaultcfg["app"]["start_hour"])
+cfg["app"].setdefault("end_hour", defaultcfg["app"]["end_hour"])
+cfg["app"].setdefault("pulse_period_m", defaultcfg["app"]["pulse_period_m"])
+cfg["stats"].setdefault("refreshperiod_d", defaultcfg["stats"]["refreshperiod_d"])
+cfg["stats"].setdefault("statperiod_d", defaultcfg["stats"]["statperiod_d"])
+cfg["colors"].setdefault("bad", defaultcfg["colors"]["bad"])
+cfg["colors"].setdefault("medium", defaultcfg["colors"]["medium"])
+cfg["colors"].setdefault("success", defaultcfg["colors"]["success"])
+cfg["colors"].setdefault("avg", defaultcfg["colors"]["avg"])
+cfg["colors"].setdefault("goal", defaultcfg["colors"]["goal"])
+cfg["colors"].setdefault("text", defaultcfg["colors"]["text"])
+cfg["colors"].setdefault("bg", defaultcfg["colors"]["bg"])
+cfg.setdefault("goals", defaultcfg["goals"])
+
+########################################################################
+
+# if outside of app hours, quit now
+if now.hour<cfg["app"]["start_hour"] or now.hour>=cfg["app"]["end_hour"]:
+	quitwithna("FREE")
+
+# download data from last Y days every once in a while
+
+refresh = False
 try:
-    with open(sdir+"/settings") as f:
-    	settings = json.loads(f.read())
-    	key = settings["key"]
-except (IOError, KeyError):
-	key = askkey()
-	with open(sdir+"/settings", "w") as f:
-			f.write(json.dumps({"key":key}))
+	with open(sdir+"/yearlystats", "r") as f:
+		data_stats = json.loads(f.read())
+	lastrefresh = datetime.datetime.fromtimestamp(data_stats["timestamp"])
+	if (now - lastrefresh).days > cfg["stats"]["refreshperiod_d"]:
+		refresh = True
+except (KeyError, IOError):
+	refresh = True
 
-# prep api call
-today = datetime.datetime.today().strftime('%Y-%m-%d')
-tomorrow = (datetime.datetime.today() + datetime.timedelta(days=1)).strftime('%Y-%m-%d')
+if refresh:
+	rb = (datetime.datetime.today() - datetime.timedelta(days=cfg["stats"]["statperiod_d"])).strftime('%Y-%m-%d')
+	try:
+		data_stats = api_request(cfg["key"], "hour", rb, today)
+		with open(sdir+"/yearlystats", "w") as f:
+			f.write(json.dumps(data_stats))
+	except URLError:
+		pass
 
-rspec = {
-	"key": key,
-	"format": "json",
-	"by": "interval",
-	"interval": "minute",
-	"rb": today,
-	"re": tomorrow,
-	"rk": "productivity"
-}
+########################################################################
 
-rstr = "https://www.rescuetime.com/anapi/data" + "?" + "&".join([k+"="+rspec[k] for k in rspec.keys()])
-r = requests.get(rstr)
-rawdata = json.loads(r.text)
+# get todays data
+try:
+	data_today = api_request(cfg["key"], "minute", today, tomorrow)
+except URLError:
+	quitwithna("404")
 
 # calc pulse: % productive time over _period_ minutes
 
-now = datetime.datetime.now()
-period = 30
-cutoff = datetime.datetime.now() - datetime.timedelta(minutes=period)
+period = cfg["app"]["pulse_period_m"]
+cutoff = now - datetime.timedelta(minutes=period)
 
 prodtime_period = 0
 prodtime_day = 0
 totaltime_day = 0
 prodtime_hours = [0]*24
-for r in rawdata["rows"]:
-	m = re.match("([0-9]+)-([0-9]+)-([0-9]+)T([0-9]+):([0-9]+):[0-9]+", r[0])
-	year = int(m[1])
-	month = int(m[2])
-	day = int(m[3])
-	hour = int(m[4])
-	minute = int(m[5])
-	time = datetime.datetime(year=year, month=month, day=day, hour=hour, minute=minute)
-	
-	totaltime_day += r[1]/60
-	if r[-1]>0:
-		prodtime_day += r[1]/60
-		prodtime_hours[time.hour] += r[1]/60
-		if time>cutoff:
-			prodtime_period += r[1]/60
+
+for r in data_today["rows"]:
+	time = datetime.datetime.strptime(r[0], "%Y-%m-%dT%H:%M:%S")
+	totaltime_day += r[-1]
+	if r[1] > 0:
+		prodtime_day += r[-1]
+		prodtime_hours[time.hour] += r[-1]
+		if time > cutoff:
+			prodtime_period += r[-1]
 
 with open(sdir+"/cache", "w") as f:
 	f.write(json.dumps(prodtime_hours))
 
-pulse_period = round(prodtime_period/period*100)
-if totaltime_day>0:
-	pulse_day = round(prodtime_day/totaltime_day*100)
+pulse_period = round(prodtime_period / period * 100)
+if totaltime_day > 0:
+	pulse_day = round(prodtime_day / totaltime_day * 100)
 else:
 	pulse_day = 0
 
 # format output
-
 if pulse_period==0:
 	periodcolor="grey"
-elif pulse_period<33:
-	periodcolor="red"
-elif pulse_period<66:
-	periodcolor="yellow"
+elif pulse_period<cfg["goals"][now.hour]/2:
+	periodcolor=cfg["colors"]["bad"]
+elif pulse_period<cfg["goals"][now.hour]:
+	periodcolor=cfg["colors"]["medium"]
 else:
-	periodcolor="green"
+	periodcolor=cfg["colors"]["success"]
 
 if pulse_day==0:
 	daycolor="grey"
 elif pulse_day<33:
-	daycolor="red"
+	daycolor=cfg["colors"]["bad"]
 elif pulse_day<66:
-	daycolor="yellow"
+	daycolor=cfg["colors"]["medium"]
 else:
-	daycolor="green"
+	daycolor=cfg["colors"]["success"]
 
 
 tooltip = "<tool>Share productive time over last {0} minutes / day</tool>".format(period)
--- chart.py
+++ chart.py
@@ -4,90 +4,164 @@ from tkinter import *
 import datetime
 import json
 import sys, os
+import subprocess
+import re
 
 sdir = os.getenv("HOME")+"/.config/xfce4-genmon-rescuetime"
+adir = "/".join(sys.path[0].split("/")[:])
+
+# load settings
+with open(adir+"/defaultcfg", "r") as f:
+		defaultcfg = json.loads(f.read())
+
+if os.path.exists(sdir+"/settings"):
+	with open(sdir+"/settings", "r") as f:
+		cfg = json.loads(f.read())
+else:
+	cfg = defaultcfg
+		
+if "key" not in cfg:
+	cfg["key"] = askkey()
+elif "key" == "":
+	cfg["key"] = askkey()
+
+cfg["app"].setdefault("start_hour", defaultcfg["app"]["start_hour"])
+cfg["app"].setdefault("end_hour", defaultcfg["app"]["end_hour"])
+cfg["app"].setdefault("pulse_period_m", defaultcfg["app"]["pulse_period_m"])
+cfg["stats"].setdefault("refreshperiod_d", defaultcfg["stats"]["refreshperiod_d"])
+cfg["stats"].setdefault("statperiod_d", defaultcfg["stats"]["statperiod_d"])
+cfg["colors"].setdefault("bad", defaultcfg["colors"]["bad"])
+cfg["colors"].setdefault("medium", defaultcfg["colors"]["medium"])
+cfg["colors"].setdefault("success", defaultcfg["colors"]["success"])
+cfg["colors"].setdefault("avg", defaultcfg["colors"]["avg"])
+cfg["colors"].setdefault("goal", defaultcfg["colors"]["goal"])
+cfg["colors"].setdefault("text", defaultcfg["colors"]["text"])
+cfg["colors"].setdefault("bg", defaultcfg["colors"]["bg"])
+cfg.setdefault("goals", defaultcfg["goals"])
 
 # load produvtivity data
 with open(sdir+"/cache", "r") as f:
-	rawdata = json.loads(f.read())
+	rawdata_today = json.loads(f.read())
+with open(sdir+"/yearlystats", "r") as f:
+	rawdata_stats = json.loads(f.read())
 
 now = datetime.datetime.now()
-prodtime_hours = rawdata[now.hour-4:now.hour+1]
-data = []
-for t,d in enumerate(prodtime_hours):
-	if t<len(prodtime_hours)-1:
-		data.append(d/60*100)
-	else:
-		data.append(d/now.minute*100)
+total_today = sum(rawdata_today)
+data_today = [rawdata_today[d]/60*100 if d<now.hour else rawdata_today[d]/(now.minute+1)*100 for d in range(len(rawdata_today))]
 
-# draw gui etc.
-def on_focus_out(event):
-	window.quit()
+# calculate mean productivity for each hour in stats
+data_stats = [[] for i in range(24)]
+N = 0
+for r in rawdata_stats["rows"]:
+	time = datetime.datetime.strptime(r[0], "%Y-%m-%dT%H:%M:%S")
+	if r[1]>0 and time.weekday()<=4:
+		#print(time, r[-1])
+		data_stats[time.hour].append(r[-1])
+#print(data_stats)
+data_mean = [sum(data_stats[h])/len(data_stats[h])/60*100 if len(data_stats[h])>0 else 0 for h in range(24)]
 
 # window settings
 margin = 5
 
 # graph settings
+left_offset = 35
+right_offset = 0
+max_bar_height = 200
+bottom_offset = 25
+top_offset = 35
+
+# bar settings
 bar_width = 20
 bar_gap = 10
-left_offset = 30+margin
-right_offset = 0+margin
-max_bar_height = 200
-bottom_offset = 20+margin
-top_offset = 30+margin
 
+# window settings
 c_height = max_bar_height + bottom_offset + top_offset
-c_width = (bar_width + bar_gap) * len(data) + left_offset + right_offset
+c_width = (bar_width + bar_gap) * max((now.hour-cfg["app"]["start_hour"]+1),6) + left_offset + right_offset
+
+# define coordinate zeros and units
+x_zero = left_offset
+y_zero = top_offset+max_bar_height
+x_unit = bar_width + bar_gap
+y_unit = max_bar_height/100
+
+def xc(x):
+	return x_zero+x_unit*x
+def yc(y):
+	return y_zero-y_unit*y
+
+# quit if focus is lost
+def on_focus_out(event):
+	window.quit()
+
+# get desired window location
+xrandr_str = subprocess.run(['xrandr | grep primary'], stdout=subprocess.PIPE, shell=True).stdout.decode('utf-8')
+xrandr_geo = xrandr_str.split(" ")[3]
+m = re.match("([0-9]+)x([0-9]+)\\+([0-9]+)\\+([0-9]+)", xrandr_geo)
+scr_width = int(m[1])
+scr_height = int(m[2])
+scr_offset_left= int(m[3])
+scr_offset_top= int(m[4])
+w_pos_x = (scr_width+scr_offset_left)*0.95-c_width
+w_pos_y = (scr_height+scr_offset_top)*0.95-c_height
 
 window = Tk()
-#window.after(3000, lambda: window.destroy())
 window.bind("<FocusOut>", on_focus_out)
-window.bind("<Leave>", on_focus_out)
-window.wm_attributes('-type', 'splash');
-
-pnt_x = window.winfo_pointerx() - window.winfo_rootx()
-pnt_y = window.winfo_pointery() - window.winfo_rooty()
-w_pos_x = pnt_x-c_width
-w_pos_y = pnt_y
+window.title("")
 window.geometry("+%d+%d" % (w_pos_x, w_pos_y))
-
-c = Canvas(window, width=c_width, height=c_height, bg='whitesmoke')
-c.pack()
 
 bold_font = font.Font(size=9, weight="bold")
 normal_font = font.Font(size=9)
+
+c = Canvas(window, width=c_width, height=c_height, bg=cfg["colors"]["bg"])
+c.pack()
+
 # draw y-axis and grid
-c.create_text(c_width/2, margin, anchor=N, text="Productivity")
+hours_str = str(round(total_today // 60)) + "h "
+min_str = str(round(total_today % 60)) + "min"
+total_str = "Productive time today: "
+if hours_str!="0h ":
+	total_str += hours_str
+total_str += min_str
+c.create_text(margin, margin, anchor="nw", text=total_str, fill=cfg["colors"]["text"])
 
 for y in range(0,101,20):
 	pos = top_offset + (max_bar_height - (y/100 * max_bar_height))
-	c.create_text(margin, pos, anchor=W, text=str(y), font=normal_font)
-	c.create_line(left_offset, pos, c_width-right_offset, pos, dash=(5, 1))
+	c.create_text(margin, pos, anchor="w", text=str(y), font=normal_font, fill=cfg["colors"]["text"])
+	c.create_line(xc(0), yc(y), c_width-right_offset, yc(y), dash=(5, 1), fill=cfg["colors"]["text"])
 
 # draw bars and x-axis
-for x, y in enumerate(data):
-	# coordinates of each bar
-	# left coordinate (from right)
-	x0 = left_offset + x*(bar_width+bar_gap)
-	# top coordinate (from top)
-	y0 = top_offset + (max_bar_height - (y/100 * max_bar_height))
-	# right coordinates (from right)
-	x1 = x0+bar_width
-	# bottom coordinates (from top)
-	y1 = top_offset + max_bar_height
-	# Draw the bar
-	if y==0:
-		color="grey"
-	elif y<33:
-		color="red"
-	elif y<66:
-		color="yellow"
-	else:
-		color = "green"
+for t, y in enumerate(data_today):
+	if t >= cfg["app"]["start_hour"] and  t <= now.hour:
+		x = t - cfg["app"]["start_hour"]
+		# coordinates of each bar
+		x0 = xc(x)
+		x1 = xc(x) + bar_width
+		y0 = yc(y) 
+		y1 = yc(0)
+		#top_offset + max_bar_height
+		# Draw the bar
+		if y==0:
+			color = "grey"
+		elif y<cfg["goals"][t]/2:
+			color = cfg["colors"]["bad"]
+		elif y<cfg["goals"][t]:
+			color = cfg["colors"]["medium"]
+		else:
+			color = cfg["colors"]["success"]
+		
+		# bar and label
+		c.create_rectangle(x0, y0, x1, y1, fill=color, outline=color)
+		c.create_text(xc(x)+bar_width/2, yc(0)+margin, anchor="n", text=str(t), font=normal_font, fill=cfg["colors"]["text"])
+		
+		# goal and avg.
+		if cfg["goals"][t]>0:
+			c.create_rectangle(x0-2, yc(cfg["goals"][t])-2, x1+2, yc(cfg["goals"][t])+2, fill=cfg["colors"]["goal"])
+		if data_mean[t]>0:
+			c.create_rectangle(x0-2, yc(data_mean[t])-2, x1+2, yc(data_mean[t])+2, fill=cfg["colors"]["avg"])
+		
 
-	c.create_rectangle(x0, y0, x1, y1, fill=color)
-	# Put the y value above the bar
-	# create text
-	c.create_text(x0 + bar_width/2, c_height, anchor=S, text=str(round(now.hour-(4-x))), font=normal_font)
+#g_plot_line(c, range(now.hour-8), data_mean[plot_start:now.hour+1])
+
+# draw goals
+
 window.mainloop()
-
